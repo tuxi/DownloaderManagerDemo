d7aceb36390fca0708f8fbb49b9ab6640f7a4ac0
Ossey <xiaoyuan1314@me.com>
2017-06-04 11:27:34 +0800

    initialize

---
+++ DownloaderManager/DownloaderManager/Sample/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DownloaderManager
+//
+//  Created by Ossey on 2017/6/4.
+//  Copyright © 2017年 Ossey. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DownloaderManager/DownloaderManager/Sample/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DownloaderManager
+//
+//  Created by Ossey on 2017/6/4.
+//  Copyright © 2017年 Ossey. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
